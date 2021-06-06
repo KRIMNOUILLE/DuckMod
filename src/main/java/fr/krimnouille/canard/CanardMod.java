59ecdc957041524adf6d267554b1d02f8b63d50c
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2021-06-06 11:39:22 +0200

    Initial commit

---
+++ src/main/java/fr/krimnouille/canard/CanardMod.java
@@ -0,0 +1,11 @@
+package fr.krimnouille.canard;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CanardMod.MODID)
+public class CanardMod {
+
+    public static final String MODID = "duck";
+
+}
